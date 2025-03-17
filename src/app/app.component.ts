50a1879e812b0aa2aabfe446d90f4a57bdf98b11
Hail-Alfisaly <hayelalfasly@gmail.com>
2025-03-17 15:58:22 +0300

    Refactor app component to improve imports and add HttpClient; update app config to include HttpClient provider; modify TaskService to fetch tasks from API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,24 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "./components/header/header.component";
-import { TasksComponent } from "./components/tasks/tasks.component";
+import { HeaderComponent } from './components/header/header.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons'; // Solid icons
-import  {HttpClient, HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [HeaderComponent, TasksComponent, FontAwesomeModule,HttpClientModule] // ✅ Import FontAwesomeModule
+  imports: [
+    HeaderComponent,
+    TasksComponent,
+    FontAwesomeModule,
+    HttpClientModule,
+  ], 
 })
 export class AppComponent {
   constructor(library: FaIconLibrary) {
-    library.addIconPacks(fas); // ✅ Register FontAwesome icons globally
+    library.addIconPacks(fas); 
   }
 }
