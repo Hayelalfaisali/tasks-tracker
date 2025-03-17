04c3ff2cc77f1afa07c12dda57d5d9592a0910a2
Hail-Alfisaly <hayelalfasly@gmail.com>
2025-03-17 15:15:56 +0300

    Add FontAwesome icons support and update task-item component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { TasksComponent } from "./components/tasks/tasks.component";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons'; // Solid icons
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  imports: [HeaderComponent, TasksComponent]
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [HeaderComponent, TasksComponent, FontAwesomeModule] // ✅ Import FontAwesomeModule
 })
 export class AppComponent {
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas); // ✅ Register FontAwesome icons globally
+  }
 }
