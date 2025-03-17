8c8b9f86fd03f02c2b3f72bfe0d73de546293280
Hail-Alfisaly <hayelalfasly@gmail.com>
2025-03-17 14:46:01 +0300

    Add tasks component with mock data and display functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { TasksComponent } from "./components/tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent]
+  imports: [HeaderComponent, TasksComponent]
 })
 export class AppComponent {
 }
